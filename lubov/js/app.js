9878857ea959534df64eed3ea3d1b6df69dff6be
Maksim Usanov <85723044+maxmillerh@users.noreply.github.com>
2023-03-05 18:27:15 +0300

    Add files via upload

--- lubov/js/app.js
+++ lubov/js/app.js
@@ -1,7 +1,118 @@
+/* тень для шапки */
+
 $(window).scroll(function() {
   $('header').toggleClass('scroll', $(this).scrollTop() > 100);
+/* 	 $('is-active').toggleClass('opac-1.style["opacity"] = 1', $(this).scrollTop() > 100); 
+ */});
+
+
+
+/* анимация появления элемента */
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+}
+let options = { threshold: [0.2] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+for (let elm of elements) {
+  observer.observe(elm);
+}
+
+/* показ нахождения на странице */
+
+function ready(fn) {
+  document.addEventListener('DOMContentLoaded', fn, false)
+}
+
+ready(() => {
+  
+  const TableOfContents = {
+    container: document.querySelector('.js-toc'),
+    links: null,
+    headings: null,
+    intersectionOptions: {
+      rootMargin: '0px',
+      threshold: 1
+    },
+    previousSection: null,
+    observer: null,
+
+    init() {
+      this.handleObserver = this.handleObserver.bind(this)
+
+      this.setUpObserver()
+      this.findLinksAndHeadings()
+      this.observeSections()
+
+    },
+
+    handleObserver(entries, observer) {
+
+      entries.forEach(entry => {
+        let href = `#${entry.target.getAttribute('id')}`,
+          link = this.links.find(l => l.getAttribute('href') === href)
+
+
+        if (entry.isIntersecting && entry.intersectionRatio >= 1) {
+          link.classList.add('is-visible')
+          this.previousSection = entry.target.getAttribute('id')
+        } else {
+          link.classList.remove('is-visible')
+        }
+
+        this.highlightFirstActive()
+      })
+    },
+
+    highlightFirstActive() {
+      let firstVisibleLink = this.container.querySelector('.is-visible')
+
+      this.links.forEach(link => {
+        link.classList.remove('is-active')
+      })
+
+      if (firstVisibleLink) {
+        firstVisibleLink.classList.add('is-active')
+      }
+
+/*       if (!firstVisibleLink && this.previousSection) {
+        this.container.querySelector(
+          `a[href="#${this.previousSection}"]`
+        ).classList.add('is-active')
+      } */
+    },
+
+    observeSections() {
+      this.headings.forEach(heading => {
+        this.observer.observe(heading)
+      })
+    },
+
+    setUpObserver() {
+      this.observer = new IntersectionObserver(
+        this.handleObserver,
+        this.intersectionOptions
+      )
+    },
+
+    findLinksAndHeadings() {
+      this.links = [...this.container.querySelectorAll('a')]
+      this.headings = this.links.map(link => {
+        let id = link.getAttribute('href')
+        return document.querySelector(id)
+      })
+    }
+  }
+
+  TableOfContents.init();
+  
 });
 
+/* смена блоков */
 
 var narash = document.getElementById('narash');
 var shugar = document.getElementById('shugar');
@@ -86,3 +197,4 @@ permonen2.onclick = function(){
   permonen2.classList.add('activete');
 
 }
+
